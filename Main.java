3ef051bb1e107fe7bd211758e38a2d1127ff1c48
rmatsumoto <rmatsumoto@fine.cs.kobe-u.ac.jp>
2022-06-13 12:13:29 +0900

    修正版

--- Main.java
+++ Main.java
@@ -5,13 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
-        int answer = rand.nextInt(101);
+        int answer = rand.nextInt(90)+10;
         System.out.println(answer);
         int user_answer=0;
         String tmp;
 
-        while(true){
-            System.out.println("数を予測して入力してください(答えは0以上100以下)");
+        int count;
+        for(count=0; count<5;count++){
+            System.out.println("残りの入力回数は"+ (5-count) + "回です。数を予測して入力してください(答えは二桁の正の整数)");
 
             tmp = scan.nextLine();
             user_answer = Integer.parseInt(tmp);
@@ -35,6 +36,10 @@ public class Main {
                     System.out.println("入力値より答えは大きいです");
                 }
             }
+        }
+
+        if(count == 5){
+            System.out.println(("Game Over!"));
         }
     
         scan.close();
